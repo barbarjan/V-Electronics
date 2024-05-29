543b0f77fbc1c7b5dd4b021cbf3618a342472ff8
barbarjan <76540587+barbarjan@users.noreply.github.com>
2024-05-29 06:37:57 +0200

    Update transmitter433.ino

--- transmitter433.ino
+++ transmitter433.ino
@@ -1,25 +1,34 @@
 /*
-  433 MHz RF Module Transmitter Demonstration 1
-  RF-Xmit-Demo-1.ino
-  Demonstrates 433 MHz RF Transmitter Module
-  Use with Receiver Demonstration 1
-
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
+  433 MHz RF module transmitter demonstration with AM2320 sensor
+  code by V Electronics JGW
+  https://www.youtube.com/@V_Electronics
 */
 
-// Include RadioHead Amplitude Shift Keying Library
 #include <RH_ASK.h>
-// Include dependant SPI Library 
 #include <SPI.h> 
+#include "Adafruit_Sensor.h"
+#include "Adafruit_AM2320.h"
+#include <string.h>
 
-// Create Amplitude Shift Keying Object
+
+// amplitude shift keying object
 RH_ASK rf_driver;
+
+// temp and hum sensor object
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
+
+char DEVICEID[] = "001";
 
 void setup()
 {
-    // Initialize ASK Object
+    Serial.begin(9600);
+    while (!Serial) {
+      delay(10); // wait until serial port opens
+    }
+
+    // Initialize Objects
     rf_driver.init();
+    am2320.begin();
 
     // Initialize builtin LED
     pinMode(LED_BUILTIN, OUTPUT);
@@ -28,15 +37,27 @@ void setup()
 
 void loop()
 {
-    Serial.println("START");
     digitalWrite(LED_BUILTIN, HIGH);
-    const char *msg = "Welcome to the Workshop!";
+    String readings = String(DEVICEID) + String(am2320.readTemperature()) + String(am2320.readHumidity());
+
+    const char *msg = readings.c_str(); //c.str() is used to convert from class String to const char*
+    Serial.println("message: ");
+    Serial.println(msg);
+    Serial.println("size: ");
+    Serial.println(strlen(msg));
+
     rf_driver.send((uint8_t *)msg, strlen(msg));
     rf_driver.waitPacketSent();
-    Serial.println("sent packet");
+
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
+
+    // debug statements
+    // Serial.println(msg);
+    // Serial.println("sent packet");
+    // Serial.print("Temp:"); Serial.println(am2320.readTemperature());
+    // Serial.print("Hum:"); Serial.println(am2320.readHumidity());
 
 
 }
